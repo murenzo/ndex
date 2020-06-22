5348bea843642a839610ed797437cc3109ab56fb
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-06-22 08:07:36 +0100

    Added logging, configured local static files and installed bootstrap

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 var express = require("express");
+var morgan = require("morgan");
+var path = require("path");
 
 var app = express();
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery")));
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/popper.js/dist/umd"))
+);
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.listen(4000, function () {
+  console.log("Listening on port 4000");
+});
